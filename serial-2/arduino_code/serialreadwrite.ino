ce6fb45775c6bde8a4a49f363fc28a8cc9f52964
Paul Tonning <paul@dotclientsolutions.com>
2018-05-22 08:06:48 -0700

    update arduino code

--- serial-2/arduino_code/serialreadwrite.ino
+++ serial-2/arduino_code/serialreadwrite.ino
@@ -1,23 +1,13 @@
 /*
-  Reading a serial ASCII-encoded string.
+  Reading and writing to a serial ASCII-encoded string.
 
-  This sketch demonstrates the Serial parseInt() function.
-  It looks for an ASCII string of comma-separated values.
-  It parses them into ints, and uses those to fade an RGB LED.
 
-  Circuit: Common-Cathode RGB LED wired like so:
-  - red anode: digital pin 3
-  - green anode: digital pin 5
-  - blue anode: digital pin 6
-  - cathode: GND
+  created: 21 May 2018
+  author: Paul Tonning 
 
-  created 13 Apr 2012
-  by Tom Igoe
-  modified 14 Mar 2016
-  by Arturo Guadalupi
-
-  This example code is in the public domain.
+  This code is in the public domain.
 */
+
 #include <Servo.h>
 
 // pins for the LEDs:
