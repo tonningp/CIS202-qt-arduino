b6e942706ef6538c8fb1c82e40af105c9b2e2832
Paul Tonning <paul@dotclientsolutions.com>
2018-05-22 08:08:49 -0700

    update arduino code

--- serial-2/arduino_code/serialreadwrite.ino
+++ serial-2/arduino_code/serialreadwrite.ino
@@ -102,40 +102,5 @@ void loop() {
     Serial.println();
   }
   
-  // if there's any serial available, read it:
-  /*
-  while (Serial.available() > 0) {
-
-    // look for the next valid integer in the incoming serial stream:
-    int red = Serial.parseInt();
-    // do it again:
-    int green = Serial.parseInt();
-    // do it again:
-    int blue = Serial.parseInt();
-
-    // look for the newline. That's the end of your sentence:
-    char endChar = Serial.read();
-    if (endChar == '\n' || endChar == '\r') {
-    //if (Serial.read() == '\r') {
-      Serial.println("Received");
-      // constrain the values to 0 - 255 and invert
-      // if you're using a common-cathode LED, just use "constrain(color, 0, 255);"
-      red =  constrain(red, 0, 255);
-      green =  constrain(green, 0, 255);
-      blue =  constrain(blue, 0, 255);
-
-      // fade the red, green, and blue legs of the LED:
-      analogWrite(redPin, red);
-      analogWrite(greenPin, green);
-      analogWrite(bluePin, blue);
-
-      // print the three numbers in one string as hexadecimal:
-      Serial.print(red, HEX);
-      Serial.print(green, HEX);
-      Serial.println(blue, HEX);
-    }
-  
-  }
-  */
   
 }
